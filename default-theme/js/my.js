2fba5e6b4d9397e51019323e69999188b28e86fb
Esmaeill <liyanboy74@gmail.com>
2022-11-03 10:25:25 +0330

    update Youtube link

--- default-theme/js/my.js
+++ default-theme/js/my.js
@@ -7,3 +7,13 @@ function toggleShowNav() {
     Nav.className = Nav.className.replace(" w3-show", "");
     }
 }
+
+function openDiv(ID) {
+    var Div = document.getElementById(ID);
+    if (Div.className.indexOf("w3-show") == -1) {
+    Div.className += " w3-show";
+    } else {
+    Div.className = Div.className.replace(" w3-show", "");
+    }
+}
+
